55af9683852173120633b3cc4e1b6a7236a14ef5
zebeid <zebeid@uvic.ca>
2025-09-14 17:27:08 -0700

    Update main_codon.py to use Codon-safe imports

--- week1/code/main_codon.py
+++ week1/code/main_codon.py
@@ -1,23 +1,21 @@
-from dbg import DBG
-from utils import read_data
+from dbg_codon import DBG   
+from utils_codon import read_data
 import sys
-import os
-
-sys.setrecursionlimit(1000000)
-
 
 if __name__ == "__main__":
     argv = sys.argv
-    short1, short2, long1 = read_data(os.path.join('./', argv[1]))
+    dataset_path = "./" + argv[1]
+    short1, short2, long1 = read_data(dataset_path)
 
     k = 25
     dbg = DBG(k=k, data_list=[short1, short2, long1])
-    # dbg.show_count_distribution()
-    with open(os.path.join('./', argv[1], 'contig.fasta'), 'w') as f:
+
+    output_file = dataset_path + "/contig.fasta"
+    with open(output_file, 'w') as f:
         for i in range(20):
             c = dbg.get_longest_contig()
             if c is None:
                 break
             print(i, len(c))
-            f.write('>contig_%d\n' % i)
+            f.write(f">contig_{i}\n")
             f.write(c + '\n')
